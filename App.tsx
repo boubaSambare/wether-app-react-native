1081469dca91b73f386e99dd84a402c2a7984526
sambare <boubasambare@yahoo.it>
2021-02-17 13:06:37 +0100

    feat: :construction:

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App(): ReactElement {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
